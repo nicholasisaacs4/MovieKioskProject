4877d39dfa68e5d9296fe4d74eb325f6eb6ddc13
nicholasisaacs4 <nicholasisaacs4@gmail.com>
2022-04-07 10:58:20 -0400

    sortbyrank

--- src/main/java/edu/ithaca/dturnbull/bank/Library.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Library.java
@@ -1,28 +1,40 @@
 package edu.ithaca.dturnbull.bank;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Library {
-    ArrayList<Movie>allMovies = new ArrayList();
-    ArrayList<Movie>topTen = new ArrayList();
+    ArrayList<Movie> allMovies = new ArrayList();
+    ArrayList<Movie> topTen = new ArrayList();
 
     public Library(){
 
     }
     
-void addMovie(Movie newMovie){ //adds movie to allMovies
-    allMovies.add(newMovie);
-}
-ArrayList getMovieList(){
-    return allMovies;
-}
+    void addMovie(Movie newMovie){ //adds movie to allMovies
+        allMovies.add(newMovie);
+    }
+    ArrayList<Movie> getMovieList(){
+        return allMovies;
+    }
 
-ArrayList getTopTen(){
-    return topTen;
-}
+    ArrayList<Movie> getTopTen(){
+        return topTen;
+    }   
 
-void sortByRank(){
+    void sortByRank(){
 
-}
+        int n = allMovies.size();  
+        for (int j = 1; j < n; j++) {  
+            int key = allMovies.get(j).ranking;  
+            int i = j-1;  
+            while ( (i > -1) && ( allMovies.get(i).ranking > key ) ) {  
+                allMovies.get(i+1).ranking = allMovies.get(i).ranking;  
+                i--;  
+            }  
+            allMovies.get(i+1).ranking  = key;  
+        }  
+
+    }
 
 }
