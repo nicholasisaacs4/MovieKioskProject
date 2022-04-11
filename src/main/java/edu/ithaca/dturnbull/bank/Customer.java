0e5e0855c5a23657b616eac5fbb967affcd74c79
beckylindquist <beckylindquist2018@gmail.com>
2022-04-11 11:33:33 -0400

    added customer paramter and some method heads

--- src/main/java/edu/ithaca/dturnbull/bank/Customer.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Customer.java
@@ -1,5 +1,50 @@
 package edu.ithaca.dturnbull.bank;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
-    
+    ArrayList<Movie> rentedMovies;
+    int IDNum;
+    String username;
+    String password;
+    double lateFees;
+    List<String> transactionHistory;
+    boolean accountStatus;
+    public Customer(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    String getUsername(){
+
+    }
+
+    void setUsername(){
+
+    }
+
+    String getPassword(){
+
+    }
+
+    void setPassword(){
+
+    }
+
+    double getLateFees(){
+
+    }
+
+    void setLateFees(){
+
+    }
+
+    Boolean getAccountStatus(){
+
+    }
+
+    void setAccountStatus(){
+
+    }
 }
