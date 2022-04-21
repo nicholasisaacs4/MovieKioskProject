e0115039ea7b3b3939030862a39365504785717c
nicholasisaacs4 <nicholasisaacs4@gmail.com>
2022-04-21 15:03:52 -0400

    kiosk methods

--- src/main/java/edu/ithaca/dturnbull/bank/KioskUI.java
+++ src/main/java/edu/ithaca/dturnbull/bank/KioskUI.java
@@ -11,12 +11,32 @@ public class KioskUI {
         this.customers = new ArrayList<>();
     }
 
-    public void login(String email, String password){
+    public String login(String email, String password){
+
+        for(int x= 0; x<= customers.size()-1; x++){
+
+            if(customers.get(x).email == email){
+                //email passes 
+                if (customers.get(x).password == password){
+                    
+                    return("Logged in");
+                    
+                }
+
+                return("Log in fail");
+            }
+
+            return("Log in fail");
+        }
+
+        return("Log in fail");
 
     }
 
     public void createAccount(String email, String password){
 
+        Customer newCust = new Customer(email, password);
+        customers.add(newCust);
 
     }
 
