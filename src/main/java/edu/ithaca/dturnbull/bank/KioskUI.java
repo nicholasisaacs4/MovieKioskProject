5b647fcecf926f920dedff13a518338be7ed73de
nicholasisaacs4 <nicholasisaacs4@gmail.com>
2022-05-04 18:35:17 -0400

    interface changes

--- src/main/java/edu/ithaca/dturnbull/bank/KioskUI.java
+++ src/main/java/edu/ithaca/dturnbull/bank/KioskUI.java
@@ -95,98 +95,125 @@ public class KioskUI {
     public void rent(Library lib){
 
         this.lib = lib;
-        System.out.println("Search by:\n1.\ttitle\n2.\tdirector\n3.\tgenre\n4.\tmost popular");
-        Scanner scanner = new Scanner(System.in);
-        // String userChoice = "1";
-        String userChoice = scanner.nextLine();
+        ArrayList<Movie> movieList = new ArrayList<>();
+
+        System.out.println("1) Search");
+        System.out.println("2) All movies");
+        Scanner scannerUser = new Scanner(System.in);
+        String Choice = scannerUser.nextLine();
         
+        if (Choice.equals("2")){
 
-        // BY TITLE //
-        if(userChoice.equals("1")){
-            System.out.println("Enter title:");
-            Scanner scanner2= new Scanner(System.in);
-            String title = scanner2.nextLine();
-            boolean movieExists = false;
-            //makes list of search results
-            ArrayList<Movie> movieList = new ArrayList<>();
             for(int x=0; x<=lib.allMovies.size()-1; x++){
-                if(lib.allMovies.get(x).title.equals(title)){
-                    movieList.add(lib.allMovies.get(x));
-                    movieExists = true;
-                }
+                System.out.println(x+") "+lib.allMovies.get(x).title+ ", "+ lib.allMovies.get(x).director+ ", "+ lib.allMovies.get(x).genre+ ", "+ lib.allMovies.get(x).price);
             }
-            //if no movies in list, initiates noMovie() (see above method)
-            if (movieExists == false){
-                noMovie();
-            }
+            Scanner scanner3= new Scanner(System.in);
+            //uses index to rent correct movie
+            System.out.println("please select a movie from above");
+            int selection = Integer.parseInt(scanner3.nextLine()) -1;
+            rent(lib.allMovies.get(selection));
 
-            //if only one movie matches search, movie is rented
-            if(movieList.size() == 1){
-                rent(movieList.get(0));
-            }
-
-            //if mult. movies in search result, user is asked to select the correct one
-            else if (movieList.size() >= 1){
-                System.out.println("Please make a selection:");
-                int i = 1;
-                //lists each movie that matches search param
-                for(int x=0; x<=movieList.size()-1; x++){
-                    System.out.println(i+".\t"+movieList.get(x).title);
-                    i++;
-                }
-                Scanner scanner3= new Scanner(System.in);
-                //uses index to rent correct movie
-                int selection = Integer.parseInt(scanner3.nextLine()) -1;
-                rent(movieList.get(selection));
-                /* 
-                 if time, add failsafe for incorrect input, and add and option to cancel search
-                 make it print (i+1)".\t<- back" or something, make if statement where index+1 = rent()
-                */
-            }  
         }
-        // BY DIRECTOR //
-        if(userChoice.equals("2")){
-            System.out.println("Enter director:");
-            Scanner scanner2= new Scanner(System.in);
-            String director = scanner2.nextLine();
-            boolean movieExists = false;
-            //makes list of search results
-            ArrayList<Movie> movieList = new ArrayList<>();
-            for(int x=0; x<=lib.allMovies.size()-1; x++){
-                if(lib.allMovies.get(x).director.equals(director)){
-                    movieList.add(lib.allMovies.get(x));
-                    movieExists = true;
+
+        if (Choice.equals("1")){
+
+            System.out.println("Search by:\n1.\ttitle\n2.\tdirector\n3.\tgenre\n4.\tmost popular\n5.\tback");
+            Scanner scanner = new Scanner(System.in);
+            // String userChoice = "1";
+            String userChoice = scanner.nextLine();
+            // BY TITLE //
+            if(userChoice.equals("1")){
+                System.out.println("Enter title:");
+                Scanner scanner2= new Scanner(System.in);
+                String title = scanner2.nextLine();
+                boolean movieExists = false;
+                //makes list of search results
+                for(int x=0; x<=lib.allMovies.size()-1; x++){
+                    if(lib.allMovies.get(x).title.equals(title)){
+                        movieList.add(lib.allMovies.get(x));
+                        movieExists = true;
+                    }
                 }
-            }
-
-            //if no movies in list, initiates noMovie() (see above method)
-            if (movieExists == false){
-                noMovie();
-            }
-
-            //if only one movie matches search, movie is rented
-            if(movieList.size() == 1){
-                rent(movieList.get(0));
-            }
-
-            //if mult. movies in search result, user is asked to select the correct one
-            else if (movieList.size() > 1){
-                System.out.println("Please make a selection:");
-                int i = 1;
-                //lists each movie that matches search param
-                for(Movie movie:movieList){
-                    System.out.println(i+".\t"+movie.title);
-                    i++;
+                //if no movies in list, initiates noMovie() (see above method)
+                if (movieExists == false){
+                    noMovie();
                 }
-                Scanner scanner3= new Scanner(System.in);
-                //uses index to rent correct movie
-                int selection = Integer.parseInt(scanner3.nextLine()) -1;
-                rent(movieList.get(selection));
-                /* 
-                 if time, add failsafe for incorrect input, and add and option to cancel search
-                 make it print (i+1)".\t<- back" or something, make if statement where index+1 = rent()
-                */
-            }  
+
+                //if only one movie matches search, movie is rented
+                if(movieList.size() == 1){
+                    rent(movieList.get(0));
+                }
+
+                //if mult. movies in search result, user is asked to select the correct one
+                else if (movieList.size() >= 1){
+                    System.out.println("Please make a selection:");
+                    int i = 1;
+                    //lists each movie that matches search param
+                    for(int x=0; x<=movieList.size()-1; x++){
+                        System.out.println(i+".\t"+movieList.get(x).title);
+                        i++;
+                    }
+                    Scanner scanner3= new Scanner(System.in);
+                    //uses index to rent correct movie
+                    int selection = Integer.parseInt(scanner3.nextLine()) -1;
+                    rent(movieList.get(selection));
+                    /* 
+                    if time, add failsafe for incorrect input, and add and option to cancel search
+                    make it print (i+1)".\t<- back" or something, make if statement where index+1 = rent()
+                    */
+                }  
+            }
+            // BY DIRECTOR //
+            if(userChoice.equals("2")){
+                System.out.println("Enter director:");
+                Scanner scanner2= new Scanner(System.in);
+                String director = scanner2.nextLine();
+                boolean movieExists = false;
+                //makes list of search results
+                for(int x=0; x<=lib.allMovies.size()-1; x++){
+                    if(lib.allMovies.get(x).director.equals(director)){
+                        movieList.add(lib.allMovies.get(x));
+                        movieExists = true;
+                    }
+                }
+
+                //if no movies in list, initiates noMovie() (see above method)
+                if (movieExists == false){
+                    noMovie();
+                }
+
+                //if only one movie matches search, movie is rented
+                if(movieList.size() == 1){
+                    rent(movieList.get(0));
+                }
+
+                //if mult. movies in search result, user is asked to select the correct one
+                else if (movieList.size() > 1){
+                    System.out.println("Please make a selection:");
+                    int i = 1;
+                    //lists each movie that matches search param
+                    for(Movie movie:movieList){
+                        System.out.println(i+".\t"+movie.title);
+                        i++;
+                    }
+                    Scanner scanner3= new Scanner(System.in);
+                    //uses index to rent correct movie
+                    int selection = Integer.parseInt(scanner3.nextLine()) -1;
+                    rent(movieList.get(selection));
+                    /* 
+                    if time, add failsafe for incorrect input, and add and option to cancel search
+                    make it print (i+1)".\t<- back" or something, make if statement where index+1 = rent()
+                    */
+                }  
+
+            }
+            if (userChoice.equals("5")){
+
+                //go back 
+            }
+        }else{
+
+
 
         }
         
@@ -195,6 +222,8 @@ public class KioskUI {
     public void rent(Movie movie){
         current_customer.rentedMovies.add(movie);
         current_customer.addToHistory("rented" + movie.getTitle() + "on" +LocalDate.now()+ "for" + movie.getPrice());
+        System.out.println("thanks for renting" + movie.title);
+
     } 
 
 }
